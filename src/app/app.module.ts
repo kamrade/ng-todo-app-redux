cbdad0fb04566f9ec326364e31b9ba89c90f00b6
Denis Michailov <kamrad.reg@gmail.com>
2017-09-19 07:02:59 +1000

    start setting up ng-redux __save__

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NgRedux, NgReduxModule } from 'ng2-redux';
 
 import { AppComponent } from './app.component';
-import { IAppState, rootReducer, INITIAL_STATE } from './store';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
+import { TodoService } from './todo.service';
+
+import { NgRedux, NgReduxModule } from 'ng2-redux';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
 
 @NgModule({
   declarations: [
@@ -21,11 +23,11 @@ import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.componen
     HttpModule,
     NgReduxModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(ngRedux: NgRedux<IAppState>){
+  constructor( ngRedux: NgRedux<IAppState> ) {
     ngRedux.configureStore(rootReducer, INITIAL_STATE);
   }
- }
+}
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NgRedux, select } from 'ng2-redux'; 
-import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO } from '../actions'; 
-import { IAppState } from '../store'; 
+import { TodoService } from '../todo.service';
+
+import { NgRedux, select } from 'ng2-redux';
+import { IAppState } from "../store";
+import { ADD_TODO, TOGGLE_TODO } from "../actions";
 
 @Component({
   selector: 'app-todo-list',
@@ -9,24 +11,43 @@ import { IAppState } from '../store';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
-  @select() todos; 
-  
-  constructor(private ngRedux: NgRedux<IAppState>) {
+  // Read the comment in TodoService
+  constructor(
+    private service: TodoService,
+    private ngRedux: NgRedux<IAppState>) {
+  }
+
+  reduxAddTodo(input) {
+    if (!input.value) return;
+
+    this.ngRedux.dispatch({
+      type: ADD_TODO,
+      newTodo: input.value
+    });
+
+    input.value = '';
+  }
+
+  reduxToggleTodo(todo) {
+    this.ngRedux.dispatch({
+      type: TOGGLE_TODO,
+      todo: todo
+    })
   }
 
   addTodo(input) {
-    if (!input.value) return; 
+    if (!input.value) return;
 
-    this.ngRedux.dispatch({ type: ADD_TODO, title: input.value });
+    this.service.addTodo(input.value);
 
     input.value = '';
   }
 
   toggleTodo(todo) {
-    this.ngRedux.dispatch({ type: TOGGLE_TODO, id: todo.id });
+    this.service.toggleTodo(todo);
   }
 
   removeTodo(todo) {
-    this.ngRedux.dispatch({ type: REMOVE_TODO, id: todo.id });
+    this.service.removeTodo(todo);
   }
 }
